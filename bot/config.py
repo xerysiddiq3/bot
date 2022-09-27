3f00efc87e50632fd9e6865ee4147f25b894a0e7
xerysiddiq3 <107870204+xerysiddiq3@users.noreply.github.com>
2022-09-27 19:21:25 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,23 +2,23 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "5719441569:AAE16ijS-J6DzpUUgTG4g-Y5hUaC1nJKoHc"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "xery_yt_uploader_bot"
 
-    API_ID = ""
+    API_ID = "10886203"
 
-    API_HASH = ""
+    API_HASH = "77fa500a7ec306b5d73044c1fb199769"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "640003406544-espn56me5ve7fm72mn5j8gu8936b2rbd.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [2118712583]
 
-    VIDEO_DESCRIPTION = ""
+    VIDEO_DESCRIPTION = "Allah Hu Akbar ☝️"
 
-    VIDEO_CATEGORY = ""
+    VIDEO_CATEGORY = "1"
 
     VIDEO_TITLE_PREFIX = ""
 
@@ -26,6 +26,6 @@ class Config:
     
     DEBUG = bool()
 
-    UPLOAD_MODE = "unlisted"
+    UPLOAD_MODE = "public"
     
     CRED_FILE = "auth_token.txt"
